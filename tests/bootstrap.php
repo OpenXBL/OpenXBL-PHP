e1ec7ceecbd901aadb6989571dffbfec0d33008d
tactics <tactics@regimbal.me>
2017-08-29 15:29:37 -0400

    2.0-beta

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+# Copyright (c) xTACTICSx
+# OpenXBL, https://xbl.io
+#
+# OpenXBL is an unofficial Xbox Live API that provides
+# user-friendly documentation, support and examples.
+#
+#
+require __DIR__ . '/../vendor/autoload.php';
