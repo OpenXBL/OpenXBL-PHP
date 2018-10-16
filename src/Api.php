765e95307231fbb8df98bc4a92d9dc1c3b059ca7
Dave <regimbal93@users.noreply.github.com>
2018-10-16 11:55:40 -0400

    Update Api.php
    Change from beta endpoint to stable prod release endpoint

--- src/Api.php
+++ src/Api.php
@@ -27,7 +27,7 @@ class Api
 	     *
 	     * @var string
 	     */
-		public $base_url = 'https://v2-beta.xbl.io/api/v2/';
+		public $base_url = 'https://xbl.io/api/v2/';
 
 	    /**
 	     * Format for expected response
